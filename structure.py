da262ef6b1b2e24f8ce676df748413988b82e3c0
Yasin Zähringer <yasinzaehringer@yhjz.de>
2013-07-19 16:54:24 +0100

    Struktur für Annexes dupliziert.

--- structure.py
+++ structure.py
@@ -94,3 +94,89 @@ class Host:
 	def __str__(self):
 		return "Host(%s)" % self.name
  
+ 
+ 
+
+class Annexes:
+	""" tracks all known annexes """
+	FILENAME = "known_annexes"
+	def __init__(self, path):
+		# save option
+		self._path = path
+		# compute the file name
+		self._annexespath = os.path.join(self._path,self.FILENAME)
+		# internal dictionary which tracks all known annexes
+		self._annexes = {}
+		# load annexes
+		self.load()
+	
+	def load(self):
+		""" loads all known annexes """
+		if os.path.isfile(self._annexespath):
+			# open the file if it exists
+			with io.open(self._annexespath, mode="rt", encoding="UTF8") as fd:
+				# decode list of annexes (json file)
+				list_of_annexes = json.load(fd)
+				# create individual annexes
+				for annex in list_of_annexes:
+					self.getAnnex(annex)
+	
+	def save(self):
+		""" saves all known annexes """
+		# get set of all known annex names
+		annexNames = self.allAnnexNames()
+		# convert it to a sorted list
+		annexNames = list(sorted(annexNames))
+
+		# open the file in write mode
+		with io.open(self._annexespath, mode="wt", encoding="UTF8") as fd:
+			# convert to string
+			s = json.dumps(annexNames, ensure_ascii=False, indent=4, sort_keys=True)
+			# dump data
+			fd.write(unicode(s))
+	
+	def allAnnexNames(self):
+		""" return all known annex names """
+		return set(self._annexes.keys())
+	
+	def allAnnexes(self):
+		""" return all known annexes """
+		return set(self._annexes.values())
+
+	def getAnnex(self, name):
+		""" get the given annex """
+
+		if name not in self._annexes:
+			# if the we do not have yet a annex with the given name, create one
+			annex = Annex(name)
+			self._annexes[name] = annex
+		
+		# return the found (or created) annex
+		return self._annexes[name]
+		
+class Annex:
+	""" encodes information of one annex """
+	def __init__(self, name):
+		# save option
+		self._name = name
+	
+	@property
+	def name(self):
+		return self._name
+	
+	#
+	# hashable type mehods, hashable is needed for dict keys and sets
+	# (source: http://docs.python.org/2/library/stdtypes.html#mapping-types-dict)
+	#
+	def __hash__(self):
+		return hash(self.name)
+	
+	def __eq__(self, other):
+		return self.name == other.name
+
+	def __repr__(self):
+		return "Annex(%r)" % self.name
+
+	def __str__(self):
+		return "Annex(%s)" % self.name
+ 
