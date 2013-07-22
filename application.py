9bb8e4f9d1461062e4d4b9a03cede6dab4fb6e81
Yasin Zähringer <yasinzaehringer@yhjz.de>
2013-07-22 11:22:03 +0100

    Der aktuelle Host kann nun abgefragt werden.

--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+import os.path
+import io
+
 import structure
 
 #
@@ -25,3 +28,29 @@ class Application:
 		self.connections = structure.Connections(self)
 		
 		
+	def currentHost(self):
+		""" get the current host """
+		# compute path
+		path = os.path.join(self.path,"current_hostname")
+		# if the path does not exist
+		if not os.path.isfile(path):
+			raise RuntimeError("Unable to find the current host.")
+		with io.open(path, mode="rt", encoding="UTF8") as fd:
+			# read
+			host = fd.read()
+			host = host.strip()
+			# associate host to a Host object
+			host = self.hosts.get(host)
+			# if we failed, raise an error
+			if host is None:
+				raise RuntimeError("Unable to find the current host.")
+			# otherwise, return the found host
+			return host
+	
+	def setCurrentHost(self, host):
+		""" set the current host """
+		# compute path
+		path = os.path.join(self.path,"current_hostname")
+		with io.open(path, mode="wt", encoding="UTF8") as fd:
+			# write
+			fd.write(host.name)
